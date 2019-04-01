016762660c6b58a3c5e4da42b39ecac45f824fcc
pradeepkadarla <kadarla.pradeep4@gmail.com>
2019-04-01 11:24:13 +0530

    a few more programs

--- src/main/java/javaPrograms/ArmStrongNumber.java
+++ src/main/java/javaPrograms/ArmStrongNumber.java
@@ -8,21 +8,18 @@ public class ArmStrongNumber {
 		System.out.println("enter the range of values");
 		Scanner s=new Scanner(System.in);
 		int n1 = s.nextInt();
-		int n2 = s.nextInt();
+
 		int arm=0;
-		int temp=0;
-		for(int i=n1;i<=n2;i++) {
-			temp=i;
-			int m=i%10;
-			arm=arm+(m*m*m);
-			i=i/10;
+		int temp=n1;
+	while(n1>0) {
+		int m=n1%10;
+		arm=arm+(m*m*m);
+		n1/=10;
 		
-			if(temp==arm) {
-				System.out.println(i);
-			}
 	}
-		
-		
+	if(arm==temp) {
+		System.out.println("given number is an armstrong number"+ temp);
+	}
 
 	}
 
